8fac15ae0487402135fb43aa08c874335619d131
Evan Sheehan <res0038@auburn.edu>
2020-05-05 17:51:34 -0400

    Now working with pixels.

--- sketch.js
+++ sketch.js
@@ -1,15 +1,21 @@
-var xoff = 0;
+
+var inc = 0.01;
 
 function setup() {
     createCanvas(400, 400);
 }
 
 function draw() {
-    background(51);
-
-    //var x = random(width);
-    var x = map(noise(xoff), 0, 1, 0, width);
-    xoff += 0.01;
-
-    ellipse(x, 200, 20);
+    loadPixels();
+    for (var x = 0; x < width; x++) {
+        for (var y = 0; y < height; y++) {
+            var randVal = random(255);
+            var index = x + y * 4;
+            pixels[index] = randVal;
+            pixels[index + 1] = randVal;
+            pixels[index + 2] = randVal;
+            pixels[index + 3] = 255;
+        }
+    }
+    updatePixels();
 }
