ab01262309e8fefcc893bdb5c8508e19779a015d
Evan Sheehan <res0038@auburn.edu>
2020-05-06 18:45:55 -0400

    2D noise implementation

--- sketch.js
+++ sketch.js
@@ -1,21 +1,29 @@
 
-var inc = 0.01;
+var inc = 0.5;
 
 function setup() {
     createCanvas(400, 400);
 }
 
+
 function draw() {
+    
+
+    var yoff = 0;
     loadPixels();
-    for (var x = 0; x < width; x++) {
-        for (var y = 0; y < height; y++) {
-            var randVal = random(255);
-            var index = x + y * 4;
-            pixels[index] = randVal;
-            pixels[index + 1] = randVal;
-            pixels[index + 2] = randVal;
+    for (var y = 0; y < width; y++) {
+        var xoff = 0;
+        for (var x = 0; x < height; x++) {
+            var r = noise(xoff, yoff) * 255
+            var index = (x + y * width) * 4;
+            pixels[index] = r;
+            pixels[index + 1] = r;
+            pixels[index + 2] = r;
             pixels[index + 3] = 255;
+
+            xoff += inc;
         }
+        yoff += inc;
     }
     updatePixels();
 }
