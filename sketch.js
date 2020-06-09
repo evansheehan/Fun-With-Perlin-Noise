5d28b28cce5ecadfc15e7e4c3abe6353184d9a4f
Evan Sheehan <res0038@auburn.edu>
2020-06-09 18:36:11 -0400

    No longer using pixel array

--- sketch.js
+++ sketch.js
@@ -1,29 +1,42 @@
 
-var inc = 0.5;
+var inc = 0.1;
+var vectorScale = 10;
+var cols, rows;
+var fr;
 
+/**
+ * 
+ */
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth*.5, windowHeight*.5);
+    pixelDensity(1);
+    cols = floor(width / vectorScale);
+    rows = floor(height / vectorScale);
+    fr = createP('');
 }
 
 
 function draw() {
-    
-
+    clear();
     var yoff = 0;
-    loadPixels();
     for (var y = 0; y < width; y++) {
         var xoff = 0;
-        for (var x = 0; x < height; x++) {
-            var r = noise(xoff, yoff) * 255
-            var index = (x + y * width) * 4;
-            pixels[index] = r;
-            pixels[index + 1] = r;
-            pixels[index + 2] = r;
-            pixels[index + 3] = 255;
+        for (var x = 0; x < cols; x++) {
+            var angle = noise(xoff, yoff) * TWO_PI;
+            //var index = (x + y * width) * 4;
+            var v = p5.Vector.fromAngle(angle);
+            //stroke(0);
+
+            push();
+            translate(x * vectorScale, y * vectorScale);
+            rotate(v.heading());
+            line(0, 0, vectorScale, 0);
+            pop();
 
             xoff += inc;
         }
         yoff += inc;
     }
-    updatePixels();
+    noLoop();
+    fr.html(floor(frameRate()));
 }
